fa5debd346ff65dd24a7beb7388302de32183703
luowei <luowei@juzix.net>
2023-03-17 10:57:51 +0800

    Update package name

--- accounts/abi/bind/template.go
+++ accounts/abi/bind/template.go
@@ -1,18 +1,18 @@
-// Copyright 2016 The go-hskchain Authors
-// This file is part of the go-hskchain library.
+// Copyright 2016 The hashkey-chain Authors
+// This file is part of the hashkey-chain library.
 //
-// The go-hskchain library is free software: you can redistribute it and/or modify
+// The hashkey-chain library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The go-hskchain library is distributed in the hope that it will be useful,
+// The hashkey-chain library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the go-hskchain library. If not, see <http://www.gnu.org/licenses/>.
+// along with the hashkey-chain library. If not, see <http://www.gnu.org/licenses/>.
 
 package bind
 
